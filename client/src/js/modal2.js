5e6e012577fb25a90e7dbe1286cf87ae750e3dc2
akita-gh <lamaro1488@gmail.com>
2023-08-25 13:07:14 +0300

    Registration page added

--- client/src/js/modal2.js
+++ client/src/js/modal2.js
@@ -1,4 +1,3 @@
-// Добавляем обработчик события для открытия модального окна при нажатии на пункт меню
 const webDevMenuItem = document.querySelector('#webDevMenuItem');
 const webDevModal = document.querySelector('#webDevModal');
 const closeModalButton = document.querySelector('#close-webDevModal');
